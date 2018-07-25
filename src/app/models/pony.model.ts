4840b9aa4c3b8ce80848adec72f801f269faf5d8
gchalas <gchalas@vega-systems.com>
2018-07-25 14:34:34 +0200

    exercise 6 100% complete

---
+++ src/app/models/pony.model.ts
@@ -0,0 +1,5 @@
+export interface PonyModel {
+    id: Number;
+    name: String;
+    color: String;
+}
--- src/app/models/race.model.ts
+++ src/app/models/race.model.ts
@@ -1,3 +1,8 @@
+import { PonyModel } from './pony.model';
+
 export interface RaceModel {
 name: String ;
+id: Number ;
+startInstant: String ;
+ponies: Array<PonyModel> ;
 }
