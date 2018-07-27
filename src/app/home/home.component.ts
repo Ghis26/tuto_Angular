639877c00bcf61e5ffb4455a64a5a5261f9c5ed5
gchalas <gchalas@vega-systems.com>
2018-07-27 15:48:40 +0200

    exercise 15 100% complete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from '../user.service';
+import { Subscription } from '../../../node_modules/rxjs';
 
 @Component({
   selector: 'pr-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  public userEventsSubscription: Subscription;
+  public user: any;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userEventsSubscription = this.userService.userEvents.subscribe((user) => this.user = user)
   }
 
+  ngOnDestroy(): void {
+    if (this.userEventsSubscription != null) {
+      this.userEventsSubscription.unsubscribe();
+    }
+  }
 }
