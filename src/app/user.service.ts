4464bf99aa85131816a4270d3a098526d4ae0e02
gchalas <gchalas@vega-systems.com>
2018-07-27 16:10:02 +0200

    exercise 16 100% complete

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -9,8 +9,10 @@ import { tap } from 'rxjs/operators';
 })
 export class UserService {
 userEvents = new BehaviorSubject<UserModel>(undefined);
+user : UserModel;
 
   constructor(private http: HttpClient) {
+    this.retrieveUser();
   }
 
   register(login: string, password: string, birthYear: number): Observable<any> {
@@ -20,7 +22,18 @@ userEvents = new BehaviorSubject<UserModel>(undefined);
 
   authenticate(credentials:{login: string; password: string}): Observable<UserModel> {
     return this.http.post<UserModel>('http://ponyracer.ninja-squad.com/api/users/authentication', credentials).pipe(
-   tap((user: UserModel) => this.userEvents.next(user))   
+   tap((user: UserModel) => this.userEvents.next(user)),
+   tap((user: UserModel) => this.storeLoggedInUser(user))
     );
+  }
+
+  storeLoggedInUser(user: UserModel){
+    this.userEvents.next(user);
+    window.localStorage.setItem("rememberMe", JSON.stringify(user));
+  }
+
+  retrieveUser(){
+    this.user = JSON.parse(window.localStorage.getItem("rememberMe"));
+    this.userEvents.next(this.user);
   }
 }
