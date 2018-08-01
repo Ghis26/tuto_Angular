2404fc947e03fd3a311cd1f81deb98b4c851e649
gchalas <gchalas@vega-systems.com>
2018-08-01 16:35:10 +0200

    exercise 27 complete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,10 @@ import { LoginComponent } from './login/login.component';
 import { BetComponent } from './bet/bet.component';
 import { LiveComponent } from './live/live.component';
 import { LoggedInGuard } from './logged-in.guard';
+import { PendingRacesComponent } from './races/pending-races/pending-races.component';
+import { FinishedRacesComponent } from './races/finished-races/finished-races.component';
+import { RacesResolverService } from './races-resolver.service';
+import { RaceResolverService } from './race-resolver.service';
 
 export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
@@ -14,10 +18,42 @@ export const ROUTES: Routes = [
     path: 'races',
     canActivate: [LoggedInGuard],
     children: [
-      { path: '', component: RacesComponent },
-      { path: ':raceId', component: BetComponent },
-      { path: ':raceId/live', component: LiveComponent }
-    ],
+      {
+        path: '',
+        component: RacesComponent,
+        children: [
+          { path: '', pathMatch: 'full', redirectTo: 'pending' },
+          {
+            path: 'pending',
+            component: PendingRacesComponent,
+            resolve: {
+              races: RacesResolverService
+            }
+          },
+          {
+            path: 'finished',
+            component: FinishedRacesComponent,
+            resolve: {
+              races: RacesResolverService
+            }
+          },
+        ]
+      },
+      {
+        path: ':raceId',
+        component: BetComponent,
+        resolve: {
+          race: RaceResolverService
+        }
+      },
+      {
+        path: ':raceId/live',
+        component: LiveComponent,
+        resolve: {
+          race: RaceResolverService
+        }
+      }
+    ]
   },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent }
